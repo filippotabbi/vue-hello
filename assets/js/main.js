752acfb43b3f5cd63b5ce202e58c937972427cff
Filippo <filippotabbi23@gmail.com>
2021-03-23 16:01:10 +0100

    esercizio terminato

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+console.log('ok');
+var root = new Vue({
+  el: '#root',
+  data: {
+    message: 'Che bello averti conosciuto,Vue!'
+  }
+});
+var img = new Vue({
+  el: '#imgvue',
+  data: {
+    img: 'https://img.favpng.com/18/13/5/vuejs-amsterdam-2019-frontend-developer-love-vue-js-graphql-javascript-png-favpng-zNqMgYfVRDDH7WYiZfzz3QBwQ.jpg'
+  }
+});
